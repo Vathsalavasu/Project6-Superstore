5908b40f2eeba9877d96f309da1bb74fd709133b
Vathsalavasu <write2vathsalar@gmail.com>
2024-10-22 18:59:00 +0530

    Seventh project

---
+++ superstores/src/main/java/com/mvc_model/superstores/SuperstoresApplication.java
@@ -0,0 +1,13 @@
+package com.mvc_model.superstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperstoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperstoresApplication.class, args);
+	}
+
+}
---
+++ superstores/src/test/java/com/mvc_model/superstores/SuperstoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc_model.superstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
